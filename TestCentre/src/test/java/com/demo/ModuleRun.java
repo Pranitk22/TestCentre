edf9f15635cf67813b73edb8c3392aa49a219f4c
7pranit <pranitkonde007@gmail.com>
2024-09-19 12:56:41 +0530

    This is my commit to run a song

---
+++ TestCentre/src/test/java/com/demo/ModuleRun.java
@@ -0,0 +1,39 @@
+package com.demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ModuleRun {
+
+public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe");
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
+		
+		driver.manage().window().maximize();
+		
+		Thread.sleep(2000);
+		
+		driver.get("https://www.youtube.com");
+		
+		Thread.sleep(3000);
+		
+		driver.findElement(By.xpath("//input[@placeholder=\"Search\"]")).sendKeys("Main tujhase aise milun");
+		
+		Thread.sleep(3000);
+		
+		driver.findElement(By.xpath("//button[@id=\"search-icon-legacy\"]")).click();
+		
+		Thread.sleep(3000);
+		
+		driver.findElement(By.xpath("(//yt-formatted-string[@class=\"style-scope ytd-video-renderer\"])[1]")).click();
+		
+		System.out.println("Test case passed");
+		
+	}
+}
